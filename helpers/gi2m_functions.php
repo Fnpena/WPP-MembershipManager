9c234a54fda10904821a1006fc62da5629381771
Fnpena <fpena@b4aconsulting.com>
2018-06-13 03:51:45 -0500

    Update Plugin Database Schema

--- helpers/gi2m_functions.php
+++ helpers/gi2m_functions.php
@@ -1,17 +1,21 @@
 <?php 
 
+/*
+	Funtion: checkPluginDataBase
+	Description: this function check current database version against the current plugin database update
+*/
 function checkPluginDataBase($db_version,$installed_version)
 {  		
       if ( $installed_version != $db_version ) 
-      {
-            return true;
-      }
+      { return true; }
       else
-      {
-            return false;
-      }
+      { return false; }
 } 
 
+/*
+	Funtion: updatePluginDataBase
+	Description: this function create the database structures
+*/
 function updatePluginDataBase()
 {
 	  global $wpdb;
@@ -22,64 +26,61 @@ function updatePluginDataBase()
       $sql = "CREATE TABLE $table_name (
             id INT NOT NULL AUTO_INCREMENT,
             code CHAR(3) NOT NULL,
-            description2 NVARCHAR(100) NOT NULL,
+            description NVARCHAR(100) NOT NULL,
             table_name NVARCHAR(15) NOT NULL,
             status CHAR(1) NOT NULL,
             UNIQUE KEY id (id))$charset_collate;";
 
       $result = maybe_create_table($table_name, $sql); 
-      //dbDelta( $sql );
     
       $table_name = $wpdb->prefix . "gi2m_members"; 
       $sql = "CREATE TABLE $table_name (
-			  member_number INT NOT NULL AUTO_INCREMENT,
-			  profile_id INT NOT NULL,			  
-			  member_avatar NVARCHAR(250),
+			  id INT NOT NULL AUTO_INCREMENT,
+			  user_id INT NOT NULL,			 
+			  membership NVARCHAR(15),				  
 			  firstname NVARCHAR(30),
 			  lastname NVARCHAR(30),
-			  motherlastname NVARCHAR(30),
-			  marriedsurname NVARCHAR(30),
+			  avatar NVARCHAR(250),
 			  blood_type NVARCHAR(2),
 			  sex NVARCHAR(1),
-			  id_card NVARCHAR(20),
+			  personal_id NVARCHAR(20),
 			  birthdate NVARCHAR(10),
-			  birthplace SMALLINT,
-			  suitability_number NVARCHAR(15),
-			  biographical_website NVARCHAR(100),
-			  email_address NVARCHAR(60),
 			  primary_contact NVARCHAR(20),
 			  secondary_contact NVARCHAR(20),
-			  personal_contact NVARCHAR(20),
-			  address_region CHAR(2),
-			  address_sub_region CHAR(3),
-			  address_sub_division CHAR(3),
-			  company_name NVARCHAR(100),
-			  company_department NVARCHAR(100),
-			  company_position NVARCHAR(50),
-			  company_address NVARCHAR(150),
-			  company_email NVARCHAR(100),
-			  company_contact_number NVARCHAR(20),
-			  college_code CHAR(2),
-              sectional CHAR(3),
-			  member_type CHAR(1),
-			  terms_accepted CHAR(1),
-              privacy_mode CHAR(1),
-			  member_status CHAR(2),
-			  guild_representation NVARCHAR(250),
-			  initial_date NVARCHAR(10),
+			  membership_type CHAR(1),
+			  register_date NVARCHAR(10),
 			  last_update datetime,
-			  unique_guid NVARCHAR(100),
-			  corporative_code NVARCHAR(20),
-			  UNIQUE KEY id (member_number)) $charset_collate;";
+              is_private CHAR(1),
+			  status CHAR(2),
+			  secret_key NVARCHAR(100),
+			  UNIQUE KEY id (id)) $charset_collate;";
     
     $result = maybe_create_table($table_name, $sql); 
+	
+	$table_name = $wpdb->prefix . 'gi2m_member_education';
+	$sql = "CREATE TABLE $table_name(
+				id INT NOT NULL AUTO_INCREMENT,
+				member_id INT NOT NULL,
+				education_lvl CHAR(2) NOT NULL,
+				description NVARCHAR(100) NOT NULL,
+				aux_code NVARCHAR(30),
+				institution NVARCHAR(100),
+				year NVARCHAR(9) NOT NULL,
+				status CHAR(1) NOT NULL,
+				UNIQUE KEY id (id))$charset_collate;";
+				
+	$result = maybe_create_table($table_name, $sql);
 
 }
 
+/*
+	Funtion: memberPreRegistration
+	Description: 
+*/
 function memberPreRegistration($user_id)
 {
 	global $wpdb;
-	$sql = "INSERT INTO {$wpdb->prefix}gi2m_members (`profile_id`,`member_status`,`member_type`) VALUES ({$user_id},'00','1')";
+	$sql = "INSERT INTO {$wpdb->prefix}gi2m_members (`user_id`,`status`,`membership_type`) VALUES ({$user_id},'00','1')";
 	$wpdb->query($sql);
 }
 
